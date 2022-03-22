be9642506e18a049eb764d3b998706450809485a
ujjwal <ujjwal.kumar@mail.mcgill.ca>
2022-03-22 09:43:57 -0400

    added mem_read and mem_write functionality for VM

--- src/core/core.c
+++ src/core/core.c
@@ -17,7 +17,7 @@ void update_flags(uint16_t r) {
 
     if(registers[r] == 0 ) { 
 
-        registers[R_COND] = FL_ZERO;  // a 1 in the left-most bit indicates negative
+        registers[R_COND] = FL_ZRO;  // a 1 in the left-most bit indicates negative
 
     }else if (registers[r] >> 15) 
     { 
@@ -27,11 +27,24 @@ void update_flags(uint16_t r) {
     }
 }
 
-void mem_write(uint16_t address, uint16_t val) { 
 
+
+// memory access ( write )
+void mem_write(uint16_t address, uint16_t val) {
+    memory[address] = val; 
 }
 
 
+// memory access ( read )
 uint16_t mem_read(uint16_t address) { 
-
+    if(address == MR_KBSR) { 
+        if(check_key()) { 
+            memory[MR_KBSR] = ( 1 << 15 ); 
+            memory[MR_KBSR] = getchar() ; 
+        }
+        else { 
+            memory[MR_KBSR] = 0 ; 
+        }
+    }
+    return memory[address]; 
 }
--- src/core/opcodes.h
+++ src/core/opcodes.h
@@ -29,13 +29,14 @@ enum {
 };
 
 // Trap codes
+// address in memory that contain trap code : 0x0 to 0x2999
 enum { 
-    TRAP_GETC = 0x20,  // get a character from the keyboard
-    TRAP_OUT  = 0x21,  // output a character
-    TRAP_PUTS = 0x22,  // output a word string
-    TRAP_IN   = 0x23,  // Input a string
+    TRAP_GETC  = 0x20,  // get a character from the keyboard, not echoed on the terminal 
+    TRAP_OUT   = 0x21,  // output a character
+    TRAP_PUTS  = 0x22,  // output a word string
+    TRAP_IN    = 0x23,  // Input a string ( get a character from temrinal echoed on the terminal )
     TRAP_PUTSP = 0x24, // output a byte string
-    TRAP_HALT = 0x25,  // halt the program 
+    TRAP_HALT  = 0x25,  // halt the program 
 };
 
 #endif
