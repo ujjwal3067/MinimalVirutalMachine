c71dfce8e77067d25acab776cf5e9972b66fe56e
ujjwal <ujjwal.kumar@mail.mcgill.ca>
2022-03-22 14:44:08 -0400

    added unix specific code for setting up terminal input

---
+++ src/core/input-buffering.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/termios.h> 
+
+
+#include "input-buffering.h"
+
+//Input buffering
+struct termios original_tio; 
+
+void disable_input_buffering() {
+    tcgetattr(STDIN_FILENO, &original_tio); 
+    struct termios new_tio = original_tio ; 
+    new_tio.c_lflag &= ~ICANON & ~ECHO; 
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering() { 
+    tcgetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
